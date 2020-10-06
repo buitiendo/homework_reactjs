8fa36efa0500d2e3d45b3de92211bd772e961961
dobt-1508 <bui.tien.do@sun-asterisk.com>
2020-10-06 11:46:01 +0700

    Updates

---
+++ precache-manifest.15c24ce1b4539777a9c80e21bb883fb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f82ecb9f016407d4ac714e91aff2c790",
+    "url": "/homework_reactjs/index.html"
+  },
+  {
+    "revision": "6342682eb6a7de2dd897",
+    "url": "/homework_reactjs/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "9ea92cd2907bc35dc24c",
+    "url": "/homework_reactjs/static/css/main.e21e6e44.chunk.css"
+  },
+  {
+    "revision": "6342682eb6a7de2dd897",
+    "url": "/homework_reactjs/static/js/2.696ceba3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/homework_reactjs/static/js/2.696ceba3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ea92cd2907bc35dc24c",
+    "url": "/homework_reactjs/static/js/main.c0ca18c5.chunk.js"
+  },
+  {
+    "revision": "b601564fc7dd1cb8819e",
+    "url": "/homework_reactjs/static/js/runtime-main.78b3862d.js"
+  }
+]);
